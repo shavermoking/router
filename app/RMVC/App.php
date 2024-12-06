0981936b61eedca2508d6a72d17ab2340adb8059
Никита Чуркин <shavermoking@gmail.com>
2024-12-06 07:04:43 +0300

    added first directory structure. Created App

---
+++ app/RMVC/App.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\RMVC;
+class App
+{
+    public static function run()
+    {
+        echo 'run';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\RMVC\App;
+
+require '../vendor/autoload.php';
+
+App::run();
