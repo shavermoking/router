3f372c0f149bf6506727fe73a1872400f097643f
Никита Чуркин <shavermoking@gmail.com>
2024-12-08 23:31:09 +0300

    working

--- app/RMVC/Route/RouteDispatcher.php
+++ app/RMVC/Route/RouteDispatcher.php
@@ -4,7 +4,10 @@ namespace App\RMVC\Route;
 
 class RouteDispatcher
 {
+    private string $requestUri = '/';
 
+
+    private array $paramMap = [];
     private RouteConfiguration $routeConfiguration;
 
     public function __construct(RouteConfiguration $routeConfiguration)
@@ -14,8 +17,37 @@ class RouteDispatcher
 
     public function process()
     {
+        $this->saveRequestUri();
+        $this->setParamMap();
+
+
+    }
+
+    private function saveRequestUri()
+    {
+        if ($_SERVER['REQUEST_URI'] !== '/') {
+            $this->requestUri = $this->clean($_SERVER['REQUEST_URI']);
+            $this->routeConfiguration->route = $this->clean($this->routeConfiguration->route);
+        }
+    }
+
+    private function clean(string $str)
+    {
+        return preg_replace('/(^\/)|(\/$)/', '', $str);
+    }
+
+    private function setParamMap()
+    {
+        $routeArray = explode('/', $this->routeConfiguration->route);
+
+        foreach ($routeArray as $paramKey => $param) {
+            if (preg_match('/{.*}/', $param)) {
+                $this->paramMap[$paramKey] = $param;
+            }
+        }
+
         echo "<pre>";
-        var_dump($_SERVER['REQUEST_URI']);
+        var_dump($routeArray);
         echo "</pre>";
     }
 }
